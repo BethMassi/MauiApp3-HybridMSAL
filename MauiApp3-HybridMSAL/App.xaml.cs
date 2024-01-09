472d9f9185b22c5fe5874fc1eea355d88e98b204
Beth Massi <bethma@microsoft.com>
2024-01-09 15:08:50 -0800

    Add project files.

---
+++ MauiApp3-HybridMSAL/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp3_HybridMSAL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
